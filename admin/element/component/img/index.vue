c3e8d927b32f1c25ce345a631b8ba638e17f6027
person-0 <13213891450@163.com>
2019-04-01 20:51:11 +0800

    feat: add img, input, mobile component on element-ui

---
+++ admin/element/component/img/index.vue
@@ -0,0 +1,55 @@
+<template>
+  <el-form-item :label="obj.title">
+        <el-upload action='' list-type="picture-card"
+              :auto-upload="false"
+              :limit='obj.limit'
+              :file-list='obj.value' 
+              :on-remove='changeImgs'
+              :on-change="changeImgs">
+
+              <i class="el-icon-plus"></i>
+
+        </el-upload>
+
+        <el-alert :title='obj.alert' :closable='false' type='error' show-icon v-if='obj.alert'></el-alert>
+  </el-form-item>
+</template>
+
+<script>
+export default {
+  name: 'img',
+
+  props: {
+    obj: {
+      type: Object
+    }
+  },
+
+  watch: {
+    obj(v1, v2){ console.error('watch'); }
+  },
+
+  data() {
+    return {
+      dialogTableVisible: false
+    }
+  },
+
+  computed: {
+    
+  },
+
+  methods: {
+    changeImgs(e, list){
+      this.obj.value = list;
+      // console.error(`change img : `, this.obj, list.length)
+
+      this.obj.alert = this.obj.value.length ? null : '请选择图片作为主图';
+    },
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ admin/element/component/mobile/index.vue
@@ -0,0 +1,46 @@
+<template>
+  <el-form-item :label="obj.title">
+        <el-input v-model="obj.value" auto-complete="off" @input='getMobile'></el-input>
+        <el-alert :title='obj.alert' :closable='false' type='error' show-icon v-if='obj.alert'></el-alert>
+  </el-form-item> 
+</template>
+
+<script>
+export default {
+  name: 'img',
+
+  props: {
+    obj: {
+      type: Object
+    }
+  },
+
+  watch: {
+    obj(v1, v2){ console.error('watch'); }
+  },
+
+  data() {
+    return {
+      dialogTableVisible: false
+    }
+  },
+
+  computed: {
+    
+  },
+
+  methods: {
+    getMobile(v){
+      let pattern = /^((13[0-9])|(14[579])|(15([0-3]|[5-9]))|(17[0135678])|(18[0-9]))\d{8}$/;
+
+      this.obj.alert = pattern.test(v) ? null : '手机号输入不正确';
+      console.error('obj : ', v, pattern.test(v));
+
+    },
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
