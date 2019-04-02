212fd31f73a51c5003d38422897e2e4f89c23089
person-0 <13213891450@163.com>
2019-04-02 20:42:15 +0800

    feat: add email, integer, number component on element-ui

---
+++ admin/element/component/integer/index.vue
@@ -0,0 +1,49 @@
+<template>
+  <el-form-item :label="obj.title">
+        <el-input v-model="obj.value" auto-complete="off" @input='getInput'></el-input>
+        <el-alert :title='obj.alert' :closable='false' type='error' show-icon v-if='obj.alert'></el-alert>
+  </el-form-item>
+</template>
+
+<script>
+export default {
+  name: 'img',
+
+  props: {
+    obj: {
+      type: Object
+    },
+  },
+
+  data() {
+    return {
+    }
+  },
+
+  computed: {
+    
+  },
+
+  methods: {
+    getInput(v){
+      let o = this.obj;
+
+      if(o.preventValidate) return ;
+
+      o.alert = o.value ? null : `请输入${o.title}`;
+
+      o.alert = isNaN(Number(o.value)) ? `${o.title}必须为数字` : null;
+
+      o.alert = o.value > 0 ? null : `${o.title}必须大于零`;
+
+      o.alert = o.value % 1 === 0 ? null : `${o.title}必须为整数`;
+      // console.error('custom input obj : ', v);
+
+    },
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ admin/element/component/number/index.vue
@@ -0,0 +1,53 @@
+<style scoped>
+.input{
+  width: 100px;
+}
+</style>
+<template>
+  <el-form-item :label="obj.title">
+    <div>
+      <span>{{obj.preValue}}</span>
+      <el-input :class='{input: obj.preValue }' v-model="obj.value" auto-complete="off" @input='getInput'></el-input>
+      <span>{{obj.postValue}}</span>
+    </div>
+        <el-alert :title='obj.alert' :closable='false' type='error' show-icon v-if='obj.alert'></el-alert>
+  </el-form-item>
+</template>
+
+<script>
+export default {
+  name: 'img',
+
+  props: {
+    obj: {
+      type: Object
+    },
+  },
+
+  data() {
+    return {
+    }
+  },
+
+  computed: {
+    
+  },
+
+  methods: {
+    getInput(v){
+      let o = this.obj;
+
+      if(o.preventValidate) return ;
+
+      o.alert = o.value ? null : `请输入${o.title}`;
+
+      o.alert = isNaN(Number(o.value)) ? `${o.title}必须为数字` : null;
+
+      o.alert = o.value > 0 ? null : `${o.title}必须大于零`;
+      // console.error('custom input obj : ', v);
+
+    },
+  }
+}
+</script>
+
