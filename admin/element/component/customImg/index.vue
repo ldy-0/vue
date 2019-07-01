5d17f6a3c607bd2d8f08567e6e7f683bc8a715a7
person-0 <13213891450@163.com>
2019-07-01 17:52:47 +0800

    refactor: element component img -> customImg, customImg add preventValidate

---
+++ admin/element/component/customImg/index.vue
@@ -0,0 +1,80 @@
+<template>
+  <el-form-item :label="obj.title">
+        <el-upload list-type="picture-card"
+              :action='obj.url' 
+              :data='obj.body'
+              :limit='obj.limit'
+              :file-list='obj.value' 
+              :on-remove='changeImgs'
+              :on-change="changeImgs" 
+              :on-success='success'
+              multiple
+              v-if='obj.url'
+              :class="{disabled:showCard}">
+              <i class="el-icon-plus"></i>
+        </el-upload>
+        <el-upload action='' list-type="picture-card" :auto-upload="false"
+              :limit='obj.limit'
+              :file-list='obj.value' 
+              :on-remove='changeImgs'
+              :on-change="changeImgs"
+              :class="{disabled:showCard}"
+               v-else>
+
+              <i class="el-icon-plus"></i>
+
+        </el-upload>
+
+        <el-alert :title='obj.alert' :closable='false' type='error' show-icon v-if='obj.alert'></el-alert>
+  </el-form-item>
+</template>
+
+<script>
+export default {
+  name: 'customImg',
+
+  props: {
+    obj: {
+      type: Object
+    }
+  },
+  computed:{
+      showCard(){
+          return  this.obj.value.length>=Number(this.obj.limit);
+      }
+  },
+  watch: {
+    obj(v1, v2){ console.error('watch'); }
+  },
+
+  data() {
+    return {
+      dialogTableVisible: false
+    }
+  },
+
+  methods: {
+
+    changeImgs(e, list){
+      let obj = this.obj;
+
+      obj.value = list;
+
+      if(obj.preventValidate) return ;
+
+      this.obj.alert = this.obj.value.length ? null : '请选择图片';
+    },
+
+    success(res){
+      let url = `${this.obj.cdnUrl}/${res.key}`;
+      // this.obj.value.push(url);
+    }
+  }
+}
+</script>
+
+<style>
+  .disabled .el-upload--picture-card{
+    display: none;
+  }
+</style>
